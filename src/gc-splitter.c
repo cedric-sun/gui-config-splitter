865e37814c24b2620d66dccf60a31de1a07ad113
Cedric Sun <sunce@ufl.edu>
2019-06-16 02:44:51 +0800

    add -C <dir> option to specify output dir

--- src/gc-splitter.c
+++ src/gc-splitter.c
@@ -3,9 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <unistd.h>
 
 /* Messages */
-#define USAGE_INFO "Usage: %s PATH_TO_GUI_CONFIG_JSON\n" 
+#define MSG_USAGE \
+"Usage: %s [options] PATH_TO_GUI_CONFIG_JSON\n"\
+"Available options are:\n"\
+"	-C <directory>		Put the output files in <directory>.\n"\
+"	-h			Print this help info.\n"
+
+#define MSG_ARG_ERR "Argument error.\n"
+#define MSG_UNK_OPT "Unknown option `-%c'\n"
 #define IO_ERROR_INFO "Failed to load file: %s, please check if it exists and its read permission\n"
 #define JSON_PARSE_ERROR_INFO "Json parse error: on line %d: %s\n"
 #define ROOT_NOT_OBJECT_INFO "Error: json root is not a json object.\n"
@@ -25,6 +33,12 @@
 /* auxiliary */
 #define READONLY "r"
 #define COVERWRITE "w"
+#define OPTSTRING "C:"
+
+extern int optind,optopt;
+extern char *optarg;
+
+static const char *progname;
 
 void get_sanitized_json_str(char *const buffer, FILE *file) {
 	char *const line = malloc(LINE_SZ);
@@ -36,18 +50,42 @@ void get_sanitized_json_str(char *const buffer, FILE *file) {
 			}
 		}
 		line[newlen] = '\0';
+		/* todo strncat */
 		strcat(buffer,line);
 	}
 	free(line);
 }
 
+void print_usage() {
+	fprintf(stderr,MSG_USAGE,progname);
+}
+
 int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		fprintf(stderr, USAGE_INFO, argv[0]);
+	progname = argv[0];
+	char c;
+	const char *out_dir;
+	while ( (c=getopt(argc,argv,OPTSTRING)) != -1) {
+		switch(c) {
+			case 'C':
+				out_dir = optarg;
+				break;
+			case '?':
+				fprintf(stderr,MSG_UNK_OPT, optopt);
+			case 'h':
+				print_usage();
+				break;
+			default:
+				return -1;
+		}
+	}
+
+	if (argc-optind !=1) {
+		fputs(MSG_ARG_ERR,stderr);
+		print_usage();
 		return -1;
 	}
 
-	const char *gc_file_path = argv[1];
+	const char *gc_file_path = argv[optind];
 
 	FILE *gc_file = fopen(gc_file_path,READONLY);
 
@@ -103,9 +141,16 @@ int main(int argc, char *argv[]) {
 		}
 		json_t *server= json_object_get(config, KEY_SERVER);
 		const char *server_str = json_string_value(server);
-		FILE *result_file = fopen(server_str, COVERWRITE);
+		char out_path[256];
+		out_path[0]='\0';
+		/* todo: strncpy, strncat*/
+		/* todo: handle file hierarchy */
+		/* TODO: add server port to output filename */
+		strcpy(out_path,out_dir);
+		strcat(out_path, server_str);
+		FILE *result_file = fopen(out_path, COVERWRITE);
 		if (!result_file){
-			fprintf(stderr, MSG_FAIL_WRITE,server_str);
+			fprintf(stderr, MSG_FAIL_WRITE,out_path);
 			return -1;
 		}
 		/* TODO: json_decref */
